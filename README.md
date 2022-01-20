db0bb67b83ee2df71a75770dbc817a4800980366
Kyle Farwell <m@kfarwell.org>
2022-01-20 02:49:29 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+rovr-static
+===========
+
+Static assets (JS, CSS, etc.) for ROVR.
+
+3rd party assets
+----------------
+
+(Not included.)
+
+* [Converse.js](https://github.com/conversejs/converse.js) (dist/)
+* [Quill](https://github.com/quilljs/quill/) (css/quill.css, js/quill.js)
+* [Tagify](https://github.com/yairEO/tagify) (css/tagify.css, js/tagify.js, js/tagify.polyfills.js)
+* [Polyfill.io Intl](https://polyfill.io/v3/polyfill.min.js?features=Intl) (js/intlpolyfill.js)
+* [libsignal-protocol-javascript](https://github.com/signalapp/libsignal-protocol-javascript) (js/libsignal-protocol.js)
+* [lottie-web](https://github.com/airbnb/lottie-web) (js/lottie.js)
+* [Flags](https://github.com/lipis/flag-icons) (img/flags/)
+
+Images of the ROVR logo and some commercially licensed images have been
+omitted.
+
+License
+-------
+
+rovr-static is copyright (c) 2019-2022 ROVR LABS INC. and is
+distributed under the AGPLv3 license. See LICENSE for details.
---
+++ js/main.js
@@ -0,0 +1,111 @@
+if (document.querySelector(".menu_items")) {
+    Array.from(document.querySelector(".menu_items").children).forEach((item, i) => {
+        item.style.transitionDelay = .47 + i / 250 + "s";
+    });
+}
+
+document.onkeydown = function(e) {
+    if (e.keyCode == 27) {
+        toggle_shazam();
+	clickme();
+    }
+};
+
+function toggle_shazam() {
+    if (!document.querySelector(".page").classList.contains("shazam")) {
+        show_shazam();
+    } else {
+        hide_shazam();
+    }
+}
+function show_shazam() {
+    document.querySelector(".page").classList.add("shazam");
+    document.querySelector(".menu_toggle").style.backgroundColor = "transparent";
+    move_notificationcount(1);
+    clickme();
+}
+function hide_shazam() {
+    document.querySelector(".page").classList.remove("shazam");
+    document.querySelector(".menu_toggle").style.backgroundColor = "var(--blueraspberry)";
+    hide_user_menu();
+    move_notificationcount(0);
+}
+
+function toggle_user_menu() {
+    if (document.querySelector(".user_menu").classList.contains("invisible")) {
+        show_user_menu();
+    } else {
+        hide_user_menu();
+    }
+}
+function show_user_menu() {
+    move_notificationcount(2);
+    document.querySelector(".user_menu").classList.remove("invisible");
+}
+function hide_user_menu() {
+    move_notificationcount(1);
+    document.querySelector(".user_menu").classList.add("invisible");
+}
+
+function move_notificationcount(pos) {
+    var count = document.getElementById("notificationcount");
+    if (count) {
+        switch(pos) {
+            case 0:
+                count.style.left = "80px";
+                count.style.top = "50px";
+                break;
+            case 1:
+                count.style.left = "53px";
+                count.style.top = "calc(100% - 80px)";
+                break;
+            case 2:
+                var btn = document.getElementById("notificationbtn");
+                count.style.left = btn.getBoundingClientRect().right - 16 + "px";
+                count.style.top = btn.getBoundingClientRect().top + 25 + "px";
+                break;
+        }
+    }
+}
+
+function external_link(event, link) {
+    event.preventDefault();
+    if (confirm("You are leaving ROVR. Are you sure you want to visit " + link.href + "? It could be sketchy!")) {
+        window.open(link.href, "_blank", "noopener");
+    }
+}
+
+function select_tab(tab, content) {
+    document.querySelector(".tabs .active").classList.remove("active");
+    tab.classList.add("active");
+
+    document.querySelector(".tab_content.active").classList.remove("active");
+    document.getElementById(content).classList.add("active");
+}
+
+function resize_tabs() {
+    if (document.querySelector(".tabs")) {
+        Array.from(document.getElementsByClassName("tabs")).forEach((tabs) => {
+            if (tabs.scrollHeight > tabs.clientHeight || tabs.scrollWidth > tabs.clientWidth) {
+                tabs.classList.add("overflown");
+            } else {
+                tabs.classList.remove("overflown");
+            }
+        });
+    }
+}
+window.addEventListener('load', function(event) {
+    resize_tabs();
+}, true);
+window.addEventListener('resize', function(event) {
+    resize_tabs();
+}, true);
+
+var clickedme = localStorage.getItem("clickedme");
+if (!clickedme) {
+    document.querySelector(".clickme").style.display = "block";
+}
+function clickme() {
+    document.querySelector(".clickme").style.display = "none";
+    localStorage.setItem("clickedme", "true");
+}
